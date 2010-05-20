bdb2b0a6670fa68257e8ddf24f2ae43277f636d2
tarlog <tarlog@b8b4894f-6750-c69b-5377-858554e2211d>
2010-05-20 20:05:54 +0000

    change cell's size functionality added

--- app/game.js
+++ app/game.js
@@ -35,6 +35,7 @@ var game = {
 	 * Binds for the key, screen, orientation change and stage event handlers.
 	 */
 	tapHandlerBind : null,
+	keyDownBind : null,
 
 	/**
 	 * Reference to the interval that runs the main game loop.
@@ -45,11 +46,11 @@ var game = {
 	 * actual data
 	 */
 	data : null,
-	
+
 	liveCellCounter : 0,
 	cols : 0,
 	rows : 0,
-	
+
 	/**
 	 * X/Y values to adjust all coordinates so things are drawn in the
 	 * playfield.
@@ -61,7 +62,7 @@ var game = {
 	/**
 	 * The game background image and its height.
 	 */
-// background : null,
+	// background : null,
 	backgroundHeight : null,
 	backgroundWidth : null,
 	liveCell : null,
@@ -71,38 +72,26 @@ var game = {
 	 * Reference to the currently opened dialog, if any.
 	 */
 	dialog : null,
-	
+
 	/**
 	 * Initialize the game. This is called once at application startup.
 	 */
 	init : function() {
 
 		Mojo.Log.info("game.init - started");
-
-		var i = null;
-
 		// Use screen height to calculate dimensions for various assets.
 		this.screenHeight = Mojo.Environment.DeviceInfo.screenHeight;
 		this.backgroundHeight = this.screenHeight - 80;
-		this.backgroundWidth = Mojo.Environment.DeviceInfo.screenWidth - (this.xAdj * 2);
-// this.gameConsole.frameHeight = this.screenHeight - 80;
+		this.backgroundWidth = Mojo.Environment.DeviceInfo.screenWidth
+				- (this.xAdj * 2);
+		// this.gameConsole.frameHeight = this.screenHeight - 80;
 
 		// init data
-		this.data = new Array();
-		this.cols = Math.floor(this.backgroundWidth / this.cellSize);
-		this.rows = Math.floor(this.backgroundHeight / this.cellSize);
-		for (x = 0;  x < this.cols; ++x) {
-			this.data[x] = new Array();
-			for (y = 0; y < this.rows; ++y) {
-				this.data[x][y] = 0;
-			}
-		}
+		this.recalculate();
 		
-		// adjust xAdj to make cells centered
-		this.xAdj = Math.floor((Mojo.Environment.DeviceInfo.screenWidth - this.cols * this.cellSize) / 2);
-		
-		// Bind and tapHandler() context.
+		// Bind events
 		this.tapHandlerBind = this.tapHandler.bind(this);
+		this.keyDownBind = this.keyDown.bind(this);
 
 		// Preload all images.
 		this.liveCell = new Image();
@@ -110,30 +99,49 @@ var game = {
 
 		this.deadCell = new Image();
 		this.deadCell.src = "images/dead-cell.png";
-		
-		
+
 		this.mainLoopBind = this.step.bind(this);
-		
+
 	}, /* End init(). */
 
+	/**
+	 * 
+	 */
+	recalculate : function() {
+		this.data = new Array();
+		this.cols = Math.floor(this.backgroundWidth / this.cellSize);
+		this.rows = Math.floor(this.backgroundHeight / this.cellSize);
+		for (x = 0; x < this.cols; ++x) {
+			this.data[x] = new Array();
+			for (y = 0; y < this.rows; ++y) {
+				this.data[x][y] = 0;
+			}
+		}
+
+		// adjust xAdj to make cells centered
+		this.xAdj = Math
+				.floor((Mojo.Environment.DeviceInfo.screenWidth - this.cols
+						* this.cellSize) / 2);
+	},
+	
 	/**
 	 * draws the background. Currently it draws the images of background, but
 	 * later it may be changed to make drawing cell-by-cell
 	 */
 	drawBackground : function() {
-// game.ctx.drawImage(game.background, game.xAdj, game.yAdj);
+		// game.ctx.drawImage(game.background, game.xAdj, game.yAdj);
 		for (x = 0; x < this.cols; ++x) {
-			for (y=0; y < this.rows; ++y) {
-				this.drawDead(x,y);
+			for (y = 0; y < this.rows; ++y) {
+				this.drawDead(x, y);
 
 			}
 		}
 	},
-	
+
 	clear : function() {
 		this.drawBackground();
 		this.liveCellCounter = 0;
-		for (x = 0;  x < this.cols; ++x) {
+		for (x = 0; x < this.cols; ++x) {
 			for (y = 0; y < this.rows; ++y) {
 				this.data[x][y] = 0;
 			}
@@ -143,18 +151,18 @@ var game = {
 		this.mainAssistant.btnStepModel.disabled = true;
 		this.mainAssistant.updateButtons();
 	},
-	
+
 	step : function() {
 		var cellsChanged = 0;
 		var newData = new Array();
-		for (x = 0;  x < this.cols; ++x) {
+		for (x = 0; x < this.cols; ++x) {
 			newData[x] = new Array();
 			for (y = 0; y < this.rows; ++y) {
-				var neighbours = this.getNeighbours(x,y);
-				if (this.data[x][y] == 1) { 
+				var neighbours = this.getNeighbours(x, y);
+				if (this.data[x][y] == 1) {
 					if (neighbours < 2 || neighbours > 3) {
 						newData[x][y] = 0;
-						this.drawDead(x,y);
+						this.drawDead(x, y);
 						this.decreaseCellCounter();
 						++cellsChanged;
 					} else {
@@ -163,7 +171,7 @@ var game = {
 				} else {
 					if (neighbours == 3) {
 						newData[x][y] = 1;
-						this.drawLive(x,y);
+						this.drawLive(x, y);
 						this.increaseCellCounter();
 						++cellsChanged;
 					} else {
@@ -173,35 +181,38 @@ var game = {
 			}
 		}
 		this.data = newData;
-		if (cellsChanged == 0 && this.gameInProgress == true && this.liveCellCounter > 0) {
+		if (cellsChanged == 0 && this.gameInProgress == true
+				&& this.liveCellCounter > 0) {
 			this.gameInProgress = false;
 			clearInterval(this.mainLoopInterval);
 			this.mainAssistant.btnClearModel.disabled = false;
 			this.mainAssistant.btnStepModel.disabled = false;
 			this.mainAssistant.btnRunModel.label = "Run";
 			this.mainAssistant.updateButtons();
-		}		
+		}
 	},
-	
-	getNeighbours : function(x,y) {
-		var left = (x > 0) ? x-1 : this.cols - 1;
-		var top = (y > 0) ? y-1 : this.rows - 1;
-		var right = (x < this.cols - 1) ? x+1 : 0;
-		var bottom = (y < this.rows - 1) ? y+1 : 0;
-		
-		return this.data[left][top] + this.data[left][y] + this.data[left][bottom] + this.data[x][top] + this.data[x][bottom] + this.data[right][top] + this.data[right][y] + this.data[right][bottom];  
+
+	getNeighbours : function(x, y) {
+		var left = (x > 0) ? x - 1 : this.cols - 1;
+		var top = (y > 0) ? y - 1 : this.rows - 1;
+		var right = (x < this.cols - 1) ? x + 1 : 0;
+		var bottom = (y < this.rows - 1) ? y + 1 : 0;
+
+		return this.data[left][top] + this.data[left][y]
+				+ this.data[left][bottom] + this.data[x][top]
+				+ this.data[x][bottom] + this.data[right][top]
+				+ this.data[right][y] + this.data[right][bottom];
 	},
-	
+
 	run : function() {
 		this.gameInProgress = true;
-	    this.mainLoopInterval = setInterval(this.mainLoopBind, 500);
+		this.mainLoopInterval = setInterval(this.mainLoopBind, 500);
 	},
-	
+
 	pause : function() {
 		this.gameInProgress = false;
 		clearInterval(this.mainLoopInterval);
 	},
-	
 
 	/**
 	 * Handle screen tap events.
@@ -227,7 +238,7 @@ var game = {
 				this.decreaseCellCounter();
 				this.data[xcell][ycell] = 0;
 				this.drawDead(xcell, ycell);
-				
+
 			} else {
 				// cell was dead, live!
 				this.increaseCellCounter();
@@ -251,7 +262,7 @@ var game = {
 			this.mainAssistant.updateButtons();
 		}
 	},
-	
+
 	increaseCellCounter : function() {
 		if (this.liveCellCounter == 0) {
 			this.mainAssistant.btnRunModel.disabled = false;
@@ -261,19 +272,44 @@ var game = {
 		}
 		this.liveCellCounter++;
 	},
-	
-	drawLive : function(x,y) {
+
+	drawLive : function(x, y) {
 		this.drawCell(this.liveCell, x, y);
 
 	},
-	
-	drawDead : function(x,y) {
+
+	drawDead : function(x, y) {
 		this.drawCell(this.deadCell, x, y);
 	},
 
-	drawCell : function(cell, xcell,ycell) {
-		this.ctx.drawImage(cell, 1 + this.xAdj + xcell
-				* this.cellSize, 1 + this.yAdj + ycell * this.cellSize, this.cellSize-1, this.cellSize-1);
+	drawCell : function(cell, xcell, ycell) {
+		this.ctx.drawImage(cell, 1 + this.xAdj + xcell * this.cellSize, 1
+				+ this.yAdj + ycell * this.cellSize, this.cellSize - 1,
+				this.cellSize - 1);
 	},
+
+	keyDown : function(inEvent) {
+		if (!this.gameInProgress && this.dialog == null) {
+			Mojo.Log.info("pressed: " + event.originalEvent.keyCode + " "
+					+ event.originalEvent.ctrlKey);
+			switch (inEvent.originalEvent.ctrlKey) {
+			case true:
+				switch (inEvent.originalEvent.keyCode) {
+				case Mojo.Char.h:
+				case Mojo.Char.h + 32:
+					this.mainAssistant.helpPressed();
+					break;
+
+				case Mojo.Char.a:
+					game.dialog = game.mainAssistant.controller.showDialog( {
+						template : "input-cell-size-dialog",
+						assistant : new InputCellAssistant(),
+						preventCancel : true
+					});
+					break;
+				}
+			}
+		}
+	}
 
 }; /* End game object. */
